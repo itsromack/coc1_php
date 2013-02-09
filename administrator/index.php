b74821b47cf610a95ba611ad9abc621bac836977
Romack Natividad <romacknatividad@gmail.com>
2013-02-09 14:03:15 -0800

    more informative admin page

--- administrator/index.php
+++ administrator/index.php
@@ -1,1 +1,26 @@
-ADMIN PAGE
+<?php
+include_once("admin_header.php");
+
+?>
+<table style="border:2px solid; background-color:silver;">
+	<tr>
+		<td>Reserved Books</td>
+		<td><?= count_reserved_books() ?></td>
+	</tr>
+	<tr>
+		<td>Borrowed Books</td>
+		<td><?= count_borrowed_books() ?></td>
+	</tr>
+	<tr>
+		<td>Active Users</td>
+		<td><?= count_active_users() ?></td>
+	</tr>
+	<tr>
+		<td>Expired Users</td>
+		<td><?= count_expired_users() ?></td>
+	</tr>
+	<tr>
+		<td>Disabled Users</td>
+		<td><?= count_disabled_users() ?></td>
+	</tr>
+</table>
